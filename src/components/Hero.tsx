ffbe14b498f197baa36d565bda407a6de9be140e
Marko Milinkovic <mamilinkovic@cetin.rs>
2026-06-16 12:50:44 +0200

    Improve global stylingss

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,19 @@
 export default function Hero() {
   return (
-    <section>
-      <h1>Zenit</h1>
+    <section
+      style={{
+        minHeight: "100vh",
+        background: "red",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "100px",
+          color: "white",
+        }}
+      >
+        HERO TEST
+      </h1>
     </section>
   );
 }
