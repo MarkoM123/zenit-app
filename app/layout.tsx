3eb51905f990d9a44d914c256d38ecc00c392d5a
Marko Milinkovic <mamilinkovic@cetin.rs>
2026-06-16 13:51:26 +0200

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,40 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
-
 export const metadata: Metadata = {
   title: "Zenit | Mikrobilje i funkcionalni sokovi",
+
   description:
     "Sveže mikrobilje i funkcionalni sokovi proizvedeni sa fokusom na kvalitet, ukus i zdrav način života.",
+
+  openGraph: {
+    title: "Zenit Mikrobilje",
+    description:
+      "Sveže mikrobilje i funkcionalni sokovi proizvedeni sa fokusom na kvalitet, ukus i zdrav način života.",
+
+    type: "website",
+
+    locale: "sr_RS",
+
+    siteName: "Zenit",
+
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Zenit Mikrobilje",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Zenit Mikrobilje",
+    description:
+      "Sveže mikrobilje i funkcionalni sokovi proizvedeni sa fokusom na kvalitet, ukus i zdrav način života.",
+    images: ["/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
